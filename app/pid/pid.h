8405e2c232a8c1334c2a5eb2b02494f6e30eb097
Jonathan Gonzaga <jonathan.s.gonzaga@gmail.com>
2020-11-08 22:10:35 -0300

    Implement pid control

--- app/pid/pid.h
+++ app/pid/pid.h
@@ -8,25 +8,25 @@
 #include "tim.h"
 #include "analog.h"
 
-// #define PID_PARAM_KP 0.25
-// #define PID_PARAM_KI 0.0001
-// #define PID_PARAM_KD 0
+#define PID_DEBUG
 
-#define PID_PARAM_KP 0.1
-#define PID_PARAM_KI 0
-#define PID_PARAM_KD 0
+#define PID_PARAM_KP 1
+#define PID_PARAM_KI 1
+#define PID_PARAM_KD 0.5
 
 #define PULSES_PER_REVOLUTION 20
-#define DEBOUNCE_TIME 1000
+#define BASET_TIME 1000
+#define MAX_PWM_OUTPUT 1000
+#define SET_POINT_INIT 150
 
-void pid_try_pid(void);
 void pid_config(void);
 void pid_process(void);
 void pid_set_parameters(uint32_t kp, uint32_t ki, uint32_t kd, uint32_t sp);
-void pid_get_parameters(uint32_t *kp, uint32_t *ki, uint32_t *kd, uint32_t *sp);
+void pid_get_parameters(int32_t *kp, int32_t *ki, int32_t *kd, int32_t *sp);
+void pid_get_data(int16_t *pv, int16_t *sp);
 
 void encoder_callback(TIM_HandleTypeDef *htim);
-void rpm_calculate(void);
 void get_rpm(uint16_t *raw_rpm);
+void get_pwm_duty(uint16_t *pwm_duty);
 
 #endif /* __PID_H */
